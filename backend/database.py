7c5577234c6c8d3719c61265b0d3d72a5c195fe9
bakalynaa <anastasiabakalyna@gmail.com>
2026-06-02 16:19:40 +0300

    feat: FastAPI setup with Supabase connection

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 def get_connection():
     return psycopg2.connect(os.getenv("DATABASE_URL"))
